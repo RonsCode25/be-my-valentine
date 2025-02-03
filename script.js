aeb5d032c9350bb7e41fe5f2018c718784332560
RonsCode25 <ronscode25@gmail.com>
2025-02-03 23:53:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.querySelector("#n").addEventListener('click',function(){
+    move();
+});
+document.querySelector("#n").addEventListener('mouseover',function(){
+    move();
+});
+
+function move(){
+    document.querySelector("#n").style.position="fixed";
+    const vw=window.innerWidth;
+    const vh=window.innerHeight;
+    const btn=document.querySelector("#n");
+
+    let curX, curY;
+
+    window.addEventListener('mousemove', function(event){
+        curX=event.clientX;
+        curY=event.clientY;
+    });
+
+    const rdloc = (() => {
+        let randX, randY;
+        const btnW = btn.offsetWidth;
+        const btnH = btn.offsetHeight;
+    
+        const maxX = (vw-btnW)+20;
+        const maxY = (vh-btnH)+20;
+    
+        do {
+            randX = Math.random() * maxX;
+            randY = Math.random() * maxY;
+        } while (Math.abs(randX - curX) < btnW && Math.abs(randY - curY) < btnH);
+    
+        return {
+            x: randX,
+            y: randY
+        };
+    })();
+    
+    const post=rdloc;
+
+    btn.style.left=`${post.x}px`;
+    btn.style.top=`${post.y}px`;
+}
+
+function change(){
+    document.querySelector(".msg").textContent="I knew you would agree!";
+    document.querySelector(".fnc").style.display="none";
+}
